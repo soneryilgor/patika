7c5524dae97f598def88b9ade5f9f2b008b3813e
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-02-26 17:27:37 +0300

    Not ortalamisini veren sınıf

---
+++ NotOrtalamasi.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+public static void main(String[] args) {
+    int mat,fizik,kimya,tarih,beden;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Matematik notunu giriniz :");
+    mat=scan.nextInt();
+
+    System.out.print("Fizik notunu giriniz :");
+    fizik=scan.nextInt();
+
+    System.out.print("Kimya notunu giriniz");
+    kimya=scan.nextInt();
+
+    System.out.print("Tarih notunu giriniz");
+    tarih=scan.nextInt();
+
+    System.out.print("Beden notunu giriniz");
+    beden=scan.nextInt();
+
+    float sonuc = ((mat+fizik+kimya+tarih+beden)/5);
+
+    System.out.println("Ortalama : " +sonuc);
+    }
+}
