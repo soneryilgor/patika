efaa7fba5402842c3bb6c70bcbafd2b6135a8f77
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-02-26 17:50:58 +0300

    sınıfı geçti sınıfta kaldi şart blokları eklendi

--- NotOrtalamasi.java
+++ NotOrtalamasi.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
 public class NotOrtalamasi {
-public static void main(String[] args) {
-    int mat,fizik,kimya,tarih,beden;
+    public static void main(String[] args) {
+        int mat, fizik, kimya, tarih, beden;
 
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-    System.out.print("Matematik notunu giriniz :");
-    mat=scan.nextInt();
+        System.out.print("Matematik notunu giriniz : ");
+        mat = scan.nextInt();
 
-    System.out.print("Fizik notunu giriniz :");
-    fizik=scan.nextInt();
+        System.out.print("Fizik notunu giriniz : ");
+        fizik = scan.nextInt();
 
-    System.out.print("Kimya notunu giriniz");
-    kimya=scan.nextInt();
+        System.out.print("Kimya notunu giriniz : ");
+        kimya = scan.nextInt();
 
-    System.out.print("Tarih notunu giriniz");
-    tarih=scan.nextInt();
+        System.out.print("Tarih notunu giriniz : ");
+        tarih = scan.nextInt();
 
-    System.out.print("Beden notunu giriniz");
-    beden=scan.nextInt();
+        System.out.print("Beden notunu giriniz : ");
+        beden = scan.nextInt();
 
-    float sonuc = ((mat+fizik+kimya+tarih+beden)/5);
+        float sonuc = ((mat + fizik + kimya + tarih + beden) / 5);
 
-    System.out.println("Ortalama : " +sonuc);
+        System.out.println("Ortalama : " + sonuc);
+
+        if (sonuc >= 60) {
+            System.out.println("Sınıfı Geçti");
+        } else if (sonuc < 60) {
+            System.out.println("Sınıfta Kaldı");
+        }
+
+        scan.close(); 
     }
 }
