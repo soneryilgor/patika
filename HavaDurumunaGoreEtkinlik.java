968ea9b7a3fc61d9d497157887cb8fd1d7cc3ea0
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-02-27 15:14:46 +0300

    Hava Sıcaklığına göre etkinlik öneren program
    Hava Sıcaklığına göre etkinlik öneren program

---
+++ HavaDurumunaGoreEtkinlik.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HavaDurumunaGoreEtkinlik {
+
+	public static void main(String[] args) {
+		int heat;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Hava Sıcaklığını Giriniz : ");
+		heat = scan.nextInt();
+		
+		if(heat<=5) {
+			System.out.println("Kayağa Gidebilirsin");
+		} else if (heat<=25){
+				if(heat<=15) {
+					System.out.println("Sinemaya Gidebilirsin");
+				}
+				if(heat>=10) {
+					System.out.println("Pikniğe Gidebilirsin");
+				}
+		}else {
+			System.out.println("Yüzmeye Gidebilirsin");
+		}
+	}
+
+}
