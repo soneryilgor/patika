59e054d73c6e42d7400d8da4f62a55259c876b9c
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-02-28 09:51:11 +0300

    Artık Yıl Hesaplayan Program
    Leap Year Calculator

---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		int year;
+		
+		Scanner scan = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        year = scan.nextInt();
+        
+        if(year%4==0 && year%100!=0 || year%400==0  ) {
+        	System.out.println(year +" bir artık yıldır !");
+        }else {
+        	System.out.println(year +" bir artık yıl değildir !");
+        }
+	}
+
+}
