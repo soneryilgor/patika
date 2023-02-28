7aa6eb216ae4802937667b9fed8c63eed4ca5702
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-02-28 11:24:41 +0300

    Sayının Katlarını Veren Program
    Sayının Katlarını Veren Program

---
+++ SayininKuvvetleri.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SayininKuvvetleri {
+
+	public static void main(String[] args) {
+		int sayi;
+		
+		Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı Giriniz : ");
+        sayi = scan.nextInt();
+        
+        for(int i=1; i<=sayi; i*=4) {
+        	System.out.print(i+" ");
+        }
+        System.out.println("\n---------");
+        for(int i=1; i<=sayi; i*=5) {
+        	System.out.print(i+" ");
+        }
+	}
+
+}
