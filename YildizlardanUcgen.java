7eb27ab2a9ad393a88444ca418c2c05cc0e03b64
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-02-28 19:57:15 +0300

    Yıldızlardan Elmas Oluşturan program

--- YildizlardanUcgen.java
+++ YildizlardanUcgen.java
@@ -17,5 +17,14 @@ public class YildizlardanUcgen {
             }
             System.out.println();
         }
+        for (int i=sayi-1; i>=1; i--) {
+            for (int j=1 ;j<=sayi-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int k=1 ; k<=(2*i)-1 ; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
