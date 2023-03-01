2f2d9084858300540df55fd06d69be071848587b
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-03-01 10:28:42 +0300

    Atm Para Çekme ve Yatırma İşlemi Yapan Program
    Atm Para Çekme ve Yatırma İşlemi Yapan Program

--- AtmIslemleri.java
+++ AtmIslemleri.java
@@ -3,42 +3,46 @@ import java.util.Scanner;
 public class AtmIslemleri {
     public static void main(String[] args) {
         String userName, password;
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int right = 3;
         int balance = 1500;
         int select;
         while (right > 0) {
             System.out.print("Kullanıcı Adınız :");
-            userName = input.nextLine();
+            userName = scan.nextLine();
             System.out.print("Parolanız : ");
-            password = input.nextLine();
+            password = scan.nextLine();
 
             if (userName.equals("patika") && password.equals("dev123")) {
                 System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
-                do {
                     System.out.println("1-Para yatırma\n" +
                             "2-Para Çekme\n" +
                             "3-Bakiye Sorgula\n" +
                             "4-Çıkış Yap");
                     System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
-                    select = input.nextInt();
+                    select = scan.nextInt();
                     switch(select){
                         case 1: System.out.print("Para miktarı : ");
-                                int price = input.nextInt();
-                                balance += price;break;
+                                int price = scan.nextInt();
+                                balance += price;
+                                System.out.println("Son Bakiyeniz : " + balance);
+                                System.out.println("Tekrar görüşmek üzere.");
+                                break;
                         case 2: System.out.print("Para miktarı : ");
-                                int price2 = input.nextInt();
+                                int price2 = scan.nextInt();
                                 if (price2 > balance) {
                                     System.out.println("Bakiye yetersiz.");
                                 } else {
                                 balance -= price2;
+                                System.out.println("Son Bakiyeniz : " + balance);
+                                System.out.println("Tekrar görüşmek üzere.");
                                 }break;
                         case 3: System.out.println("Bakiyeniz : " + balance);
                         break;
+                        case 4:System.out.println("Tekrar görüşmek üzere.");
+                        break;
                     }
-                } while (select != 4);
-                System.out.println("Tekrar görüşmek üzere.");
-                break;
+                
             } else {
                 right--;
                 System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
@@ -47,8 +51,7 @@ public class AtmIslemleri {
                     } else {
                         System.out.println("Kalan Hakkınız : " + right);
                 }
-            }
-        }
-    }
-        
+            }break;
+        }scan.close();
+    }  
 }
