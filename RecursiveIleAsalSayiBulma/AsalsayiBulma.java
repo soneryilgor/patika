261a634deecc63af17d4524f4ca1ec82a932efbc
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2023-03-11 15:09:19 +0300

    Recursive ile asal sayı bulma

---
+++ RecursiveIleAsalSayiBulma/AsalsayiBulma.java
@@ -0,0 +1,26 @@
+package RecursiveIleAsalSayiBulma;
+
+import java.util.Scanner;
+
+public class AsalsayiBulma {
+    public static boolean asalMi(int sayi, int a) {
+        if (a == 1) {
+            return true;
+        } else if (sayi % a == 0) {
+            return false;
+        } else {
+            return asalMi(sayi, a - 1);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sayı girin: ");
+        int sayi = sc.nextInt();
+
+        if (asalMi(sayi, sayi/2)) {
+            System.out.println(sayi + " Asal Sayıdır.");
+        } else {
+            System.out.println(sayi + " Asal Değildir.");
+        }
+    }
+}
